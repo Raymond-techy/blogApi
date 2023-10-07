2c1449d8e8a7fbabf163aab23f6279ef64e03963
Raymond-techy <adunoloyerahmantaiwo200208@gmail.com>
2023-10-07 13:17:45 -0700

    set up for prod

--- startup/config.js
+++ startup/config.js
@@ -2,6 +2,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
   MONGO_URL: process.env.MONGO_DB,
-  JWT_KEY: process.env.JWT_KEY,
+  JWT_KEY: process.env.JWT_PRIVATEKEY,
   port: process.env.PORT,
 };
